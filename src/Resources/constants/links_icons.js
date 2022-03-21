744cdadfdc9cce73b72c7f62061981da3c5d5564
ivan.dimitrov <ivan.dimitrov@digital-mobility-solutions.de>
2022-03-21 16:56:42 +0100

    make it better

---
+++ src/Resources/constants/links_icons.js
@@ -0,0 +1,23 @@
+import {ReactComponent as GITHUB} from '../icons/github.svg'
+import {ReactComponent as INSTAGRAM} from '../icons/instagram.svg'
+import {ReactComponent as TWITTER} from '../icons/twitter.svg'
+import {ReactComponent as LINKEDIN} from '../icons/linkedin.svg'
+import {ReactComponent as CODEPEN} from '../icons/codepen.svg'
+import {ReactComponent as DISCORD} from '../icons/discord1a.svg'
+
+const links_icons =
+[
+    {
+        link : 'https://www.instagram.com',
+        icon : INSTAGRAM
+    },
+    {
+        link : 'https://twitter.com',
+        icon : TWITTER
+    }, 
+    {
+        link : 'https://discord.com',
+        icon : DISCORD
+    }
+]
+export default links_icons
