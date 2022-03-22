a1efb34f5873fe269db7bdaf28d007166362a789
ivan.dimitrov <ivan.dimitrov@digital-mobility-solutions.de>
2022-03-22 14:37:18 +0100

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -37,10 +37,8 @@ module.exports = env => {
 	let BUILD_DIRECTORY_PREFIX;
 
 	if (env) {
-		//CLIENT_PORT = env.CLIENT_PORT;
-		//SERVER_PORT = env.SERVER_PORT;
-		CLIENT_PORT = process.env.PORT || 3000
-		SERVER_PORT = process.env.PORT || 3000
+		CLIENT_PORT = env.CLIENT_PORT;
+		SERVER_PORT = env.SERVER_PORT;
 		CLIENT_LOG_LEVEL = JSON.stringify(env.CLIENT_LOG_LEVEL || 'ERROR');
 		BUILD_DIRECTORY_PREFIX = env.BUILD_DIRECTORY_PREFIX;
 	}
